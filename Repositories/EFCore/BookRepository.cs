d1cf101b2d0c84e8408c0a3eb6004d02768563dc
Alperen Güneş <alperenmanavgat@outlook.com>
2024-07-11 23:56:39 +0300

    Repository and repository manager

---
+++ Repositories/EFCore/BookRepository.cs
@@ -0,0 +1,30 @@
+﻿using Entities.Models;
+using Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.EFCore
+{
+    public class BookRepository : RepositoryBase<Book>, IBookRepository
+    {
+        public BookRepository(RepositoryContext context) : base(context)
+        {
+        }
+
+        public void CreateOneBook(Book book) => Create(book);
+
+        public void DeleteOneBook(Book book) => Delete(book);
+       
+
+        public IQueryable<Book> GetAllBooks(bool trackChanges) => FindAll(trackChanges);
+       
+
+        public IQueryable<Book> GetBookById(int d, bool trackChanges) => FindByCondition(b => b.Id == d, trackChanges);
+
+        public void UpdateOneBook(Book book) => Update(book);
+
+    }
+}
