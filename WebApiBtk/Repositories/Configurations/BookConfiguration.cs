9550fdae390dac1148c1dd829b62e5ada1073d3a
Alperen Güneş <alperenmanavgat@outlook.com>
2024-07-11 17:30:42 +0300

    Repository Context and Migrations

---
+++ WebApiBtk/Repositories/Configurations/BookConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WebApiBtk.Models;
+
+namespace WebApiBtk.Repositories.Configurations
+{
+    public class BookConfiguration : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.HasData(
+
+                new Book
+                {
+                    Id = 1,
+                    Title = "Book 1",
+                    Price = 100
+                },
+                new Book
+                {
+                    Id = 2,
+                    Title = "Book 11",
+                    Price = 10000
+                },
+                new Book
+                {
+                    Id = 3,
+                    Title = "Book 10",
+                    Price = 1000
+                });
+        }
+    }
+}
---
+++ WebApiBtk/Repositories/RepositoryContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiBtk.Models;
+
+namespace WebApiBtk.Repositories
+{
+
+
+    public class RepositoryContext: DbContext
+    {
+        public RepositoryContext(DbContextOptions options) : base(options)
+        { 
+        }
+        public DbSet<Book> Books { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new Configurations.BookConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
