469764c0c19137e6efa4c264d840d3e2bf9ba06f
Alperen Güneş <alperenmanavgat@outlook.com>
2024-07-12 20:28:59 +0300

    Custom 'notfound' Exception

---
+++ Entities/Exceptions/BookNotFound.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities.Exceptions
+{
+    public sealed class BookNotFound : NotFound
+    {
+        public BookNotFound(int id) : base($"The book with id: {id} could not found ")
+        {
+        }
+    }
+}
