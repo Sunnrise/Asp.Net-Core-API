626f9af93f0b53ceb942cfc37726c90da4c689ff
Alperen Güneş <alperenmanavgat@outlook.com>
2024-07-12 11:23:42 +0300

    Logging service and logger manager

---
+++ Services/LoggerManager.cs
@@ -0,0 +1,26 @@
+﻿using NLog;
+using Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class LoggerManager : ILoggerService
+    {
+        private static ILogger _logger=LogManager.GetCurrentClassLogger();
+        public void LogDebug(string message)=> _logger.Debug(message);
+
+
+        public void LogError(string message) => _logger.Error(message);
+        
+
+        public void LogInfo(string message) => _logger.Info(message);
+        
+
+        public void LogWarning(string message) => _logger.Warn(message);
+        
+    }
+}
