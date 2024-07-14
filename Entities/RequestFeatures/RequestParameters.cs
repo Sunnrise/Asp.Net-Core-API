938c76f67b64a814033dffb358c9f5a337f5ce7a
Alperen Güneş <alperenmanavgat@outlook.com>
2024-07-14 21:25:17 +0300

    Request Parameters base class and book parameters class

---
+++ Entities/RequestFeatures/RequestParameters.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.RequestFeatures
+{
+    public abstract class RequestParameters
+    {
+        const int maxPageSize = 50;
+
+        //Auto-implemented properties
+        public int PageNumber { get; set; } = 1;
+
+        //Full-property
+        private int _pageSize = 10;
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = (value > maxPageSize) ? maxPageSize : value;
+        }
+    }
+}
