af5c99740728aaa061511ec72f64a420e9cc15fa
Alperen Güneş <alperenmanavgat@outlook.com>
2024-07-15 21:08:26 +0300

    IBookLink and Its concrete class

---
+++ Services/Contracts/IBookLinks.cs
@@ -0,0 +1,18 @@
+﻿using Entities.DataTransferObject;
+using Entities.LinkModels;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Contracts
+{
+    public interface IBookLinks
+    {
+        public LinkResponse TryGenerateLinks(IEnumerable<BookDto> bookDto,
+            string fields, HttpContext httpContext);
+       
+    }
+}
