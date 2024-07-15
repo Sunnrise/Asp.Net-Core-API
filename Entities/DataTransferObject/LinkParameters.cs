8e686979cd238152640eb9905739fb879ffd2634
Alperen Güneş <alperenmanavgat@outlook.com>
2024-07-15 21:12:45 +0300

    LinkParameter DTO

---
+++ Entities/DataTransferObject/LinkParameters.cs
@@ -0,0 +1,14 @@
+﻿using Entities.RequestFeatures;
+using Microsoft.AspNetCore.Http;
+
+
+namespace Entities.DataTransferObject
+{
+    public record LinkParameters
+    {
+        
+
+        public BookParameters BookParameters { get; init; }
+        public HttpContext HttpContext { get; init; }
+    }
+}
