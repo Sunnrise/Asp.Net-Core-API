b87db62d11ee0059a6e6c06fdaa97b43df794d20
Alperen Güneş <alperenmanavgat@outlook.com>
2024-07-15 17:02:17 +0300

    Entity, Shape Entity, LinkModel Creation

---
+++ Entities/LinkModels/LinkCollectionWrapper.cs
@@ -0,0 +1,15 @@
+﻿namespace Entities.LinkModels
+{
+    public class LinkCollectionWrapper<T> : LinkResourceBase
+    {
+        public List<T> Value { get; set; } = new();
+        public LinkCollectionWrapper()
+        {
+            Value = new List<T>();
+        }
+        public LinkCollectionWrapper(List<T> value)
+        {
+            Value = value;
+        }
+    }
+}
