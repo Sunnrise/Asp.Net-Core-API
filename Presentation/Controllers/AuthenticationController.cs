e2de5524d1a31d498a15e1021b86874c215ff93c
Alperen Güneş <alperenmanavgat@outlook.com>
2024-07-16 23:01:17 +0300

    AuthenticationController and RegisterUser endpoint

---
+++ Presentation/Controllers/AuthenticationController.cs
@@ -0,0 +1,42 @@
+﻿using Entities.DataTransferObject;
+using Microsoft.AspNetCore.Mvc;
+using Presentation.ActionFilters;
+using Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/authentication")]
+    public class AuthenticationController:ControllerBase
+    {
+        private readonly IServiceManager _serviceManager;
+
+        public AuthenticationController(IServiceManager serviceManager)
+        {
+            _serviceManager = serviceManager;
+        }
+
+        [HttpPost]
+        [ServiceFilter(typeof(ValidationFilterAttribute))]
+        public async Task<IActionResult> RegisterUser([FromBody]UserForRegistrationDto userForRegistrationDto)
+        {
+            var result= await _serviceManager
+                .AuthenticationService
+                .RegisterUser(userForRegistrationDto);
+            if(!result.Succeeded)
+            {
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError(error.Code, error.Description);
+                }
+                return BadRequest(ModelState);
+            }
+            return StatusCode(201);
+        }
+    }
+}
