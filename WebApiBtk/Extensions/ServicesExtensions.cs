0ef2eb1d60f46fbfbaf8f967477f52886806a633
Alperen Güneş <alperenmanavgat@outlook.com>
2024-07-16 13:43:21 +0300

    Versioning Extension Method

--- WebApiBtk/Extensions/ServicesExtensions.cs
+++ WebApiBtk/Extensions/ServicesExtensions.cs
@@ -85,6 +85,15 @@ namespace WebApiBtk.Extensions
                 }
             });
         }
+        public static void ConfigureVersioning(this IServiceCollection services)
+        {
+            services.AddApiVersioning(opt =>
+            {
+                opt.ReportApiVersions = true;
+                opt.AssumeDefaultVersionWhenUnspecified = true;
+                opt.DefaultApiVersion = new ApiVersion(1, 0);
+            });
+        }  
     }
    
 }
