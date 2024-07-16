3c9cb1912c0482518d4a6c6bb56d6302e30bdac3
Alperen Güneş <alperenmanavgat@outlook.com>
2024-07-16 14:02:27 +0300

    Version2 controller and update service and repository

---
+++ Presentation/Controllers/BooksV2Controller.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Controllers
+{
+    [ApiVersion("2.0")]
+    [ApiController]
+    [Route("api/books")]
+    public class BooksV2Controller : ControllerBase
+    {
+        private readonly IServiceManager _manager;
+
+        public BooksV2Controller(IServiceManager manager)
+        {
+            _manager = manager;
+        }
+
+        [HttpGet(Name = "GetAllBooksAsync")]
+        public async Task<IActionResult> GetAllBooksAsync()
+        {
+            var books= await _manager.BookService.GetAllBooksAsync(false);
+            return Ok(books);
+        }
+    }
+}
